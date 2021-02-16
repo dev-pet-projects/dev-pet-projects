feccf7977d1f5b299296459cbd54aa3cad61cfe6
madmatvey <madmatvey@users.noreply.github.com>
2021-02-16 09:00:04 +0000

    deploy: dev-pet-projects/pet-projects.dev-frontend@b6f42975dbf17165880331096b3882037a5dfe33

---
+++ _nuxt/static/1613465996/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613465996",layout:"default",error:a,state:{orgRepos:[],dataBaseFields:["orgRepos"]},serverRendered:true,routePath:"\u002Finspire",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8d97fd2c"}}}}(null));
